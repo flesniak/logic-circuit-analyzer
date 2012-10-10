fbd22d7ebfa03dfd79bca330efdbf202fcd336db
Fabian Lesniak <fabian.lesniak@student.kit.edu>
2012-10-10 17:44:28 +0200

    graphanalyzer fully working

--- graphanalyzer.cpp
+++ graphanalyzer.cpp
@@ -5,9 +5,10 @@
 #include "graphcreator.h"
 #include "gateelement.h"
 #include "listelement.h"
+#include "flipflop.h"
 
-#include <iostream>
-using namespace std;
+//#include <iostream>
+//using namespace std;
 
 GraphAnalyzer::GraphAnalyzer(Library* library, Factors* factors, GraphCreator* graphCreator)
     : p_library(library), p_factors(factors), p_graphCreator(graphCreator), p_outputPathRuntime(0), p_transitionPathRuntime(0)
@@ -28,7 +29,6 @@ bool GraphAnalyzer::analyze()
     for( ListElement* element = p_graphCreator->getFirstElement(); element != 0; element = element->getNextElement() )
         if( element->getGateElement()->getIsInputElement() || element->getGateElement()->getGateType()->getIsFlipflop() ) { //valid start point
             p_dfsCache.clear(); //reset all cached data
-            //cout << "DEBUG: Init doDfs for " << element->getGateElement()->getName() << endl;
             doDfs(element->getGateElement(), element->getGateElement());
         }
 	return true;
@@ -38,18 +38,13 @@ void GraphAnalyzer::doDfs(GateElement* element, GateElement* start)
 {
     vector<GateElement*> successors = element->getSuccessors();
     for(vector<GateElement*>::iterator it = successors.begin(); it != successors.end(); it++)
-            //cout << "DEBUG: processing successor " << (*it)->getName() << " of " << element->getName() << endl;
         if( (*it)->getGateType()->getIsFlipflop() ) {
-            //cout << "DEBUG: entering flipflop handling..." << endl;
             if( p_transitionPathRuntime < element->getBaseRuntime() + p_dfsCache[element].pathRuntime ) {
                 p_transitionPathRuntime = element->getBaseRuntime() + p_dfsCache[element].pathRuntime;
                 p_transitionPath = createSequenceString(element,start) + "->" + (*it)->getName(); //*it not in dfsCache yet, add it manually
-                    cout << "DEBUG: new transition " << p_transitionPathRuntime << endl;
             }
         } else {
-                //cout << "DEBUG: entering default handling..." << endl;
             if( p_dfsCache[*it].pathRuntime < p_dfsCache[element].pathRuntime + element->getBaseRuntime() ) {
-                    //cout << "DEBUG: pathRuntime smaller, increasing..." << endl;
                 if( ( p_dfsCache[*it].pathRuntime || *it == start ) && p_dfsCache[*it].predecessor != element ) {
                     p_dfsCache[*it].predecessor = element;
                     //if(  ) do cycleSearch
@@ -60,12 +55,9 @@ void GraphAnalyzer::doDfs(GateElement* element, GateElement* start)
             }
         }
     if( element->getIsOutputElement() && p_outputPathRuntime < p_dfsCache[element].pathRuntime + element->getBaseRuntime() ) {
-            //cout << "DEBUG: output element handling..." << endl;
         p_outputPathRuntime = p_dfsCache[element].pathRuntime + element->getBaseRuntime();
         p_outputPath = createSequenceString(element,start);
-            cout << "DEBUG: new output " << p_outputPathRuntime << endl;
     }
-        //cout << "DEBUG: element " << element->getName() << " complete" << endl;
 }
 
 string GraphAnalyzer::createSequenceString(GateElement* last, GateElement* first)
@@ -78,12 +70,17 @@ string GraphAnalyzer::createSequenceString(GateElement* last, GateElement* first
 
 bool GraphAnalyzer::calculateBaseTimes()
 {
-    double p_tempFactors = p_factors->getTempFactor()*p_factors->getVoltageFactor()*p_factors->getProcessFactor();
-    for(ListElement* element = p_graphCreator->getFirstElement(); element != 0; element = element->getNextElement())
-        element->getGateElement()->setBaseRuntime( p_tempFactors *
-                                                  (element->getGateElement()->getGateType()->getBaseTime() +
+    double tempFactors = p_factors->getTempFactor()*p_factors->getVoltageFactor()*p_factors->getProcessFactor();
+    for(ListElement* element = p_graphCreator->getFirstElement(); element != 0; element = element->getNextElement()) {
+        vector<GateElement*> successors = element->getGateElement()->getSuccessors();
+        double cLoad = 0;
+        for(vector<GateElement*>::iterator it = successors.begin(); it != successors.end(); it++)
+            cLoad += (*it)->getGateType()->getLoadCapacity();
+        element->getGateElement()->setBaseRuntime( tempFactors *
+                                                 ( element->getGateElement()->getGateType()->getBaseTime() +
                                                    element->getGateElement()->getGateType()->getLoadFactor() *
-                                                   element->getGateElement()->getGateType()->getLoadCapacity() / 1000.0) );
+                                                   cLoad / 1000.0 ) );
+    }
     return true;
 }
 
@@ -109,5 +106,7 @@ double GraphAnalyzer::getTransitionPathRuntime() const
 
 double GraphAnalyzer::getMaxFrequency() const
 {
-    return 0;
+    if( p_transitionPathRuntime == 0 )
+        return 0;
+    return 1.0e12/( p_transitionPathRuntime + static_cast<const Flipflop*>(p_library->getGate("dff"))->getSetupTime() );
 }
